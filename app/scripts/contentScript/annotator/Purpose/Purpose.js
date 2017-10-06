16523f8c7ef21cbcf9139f511ce44e1f1dc9436b
Haritz Medina <haritzmedina@gmail.com>
2017-10-06 18:40:27 +0200

    Created sidebar menu for annotator:Purpose

--- app/scripts/contentScript/annotator/Purpose/Purpose.js
+++ app/scripts/contentScript/annotator/Purpose/Purpose.js
@@ -1,4 +1,5 @@
 const HypothesisClient = require('../../../hypothesis/HypothesisClient')
+const $ = require('jquery')
 require('bootstrap')
 
 class Purpose {
@@ -8,10 +9,53 @@ class Purpose {
 
   init () {
     console.debug('Initializing purpose annotator')
-    // TODO Create a lateral panel
-    document.addEventListener('select', (e) => {
+    // Create sidebar
+    this.initSidebar()
+    document.addEventListener('selectionchange', (e) => {
       console.log(e)
     })
+  }
+
+  initSidebar () {
+    this.initSidebarButton()
+    let sidebarURL = chrome.extension.getURL('pages/annotator/Purpose/sidebar.html')
+    $.get(sidebarURL, (html) => {
+      $('body').append($.parseHTML(html))
+      chrome.runtime.sendMessage({scope: 'hypothesis', cmd: 'getToken'}, (token) => {
+        if (token) {
+          // TODO Retrieve groups if user is logged in
+          // TODO If logged in, load groups of hypothesis the user is enrolled in
+          // TODO Retrieve annotations for url https://*/* and tag prefix purpose:
+          // TODO Periodically retrieve annotations and reload layout
+        }
+      })
+    })
+  }
+
+  initSidebarButton () {
+    let sidebarButton = document.createElement('button')
+    sidebarButton.id = 'sidebarButton'
+    let iconBar = document.createElement('span')
+    iconBar.className = 'sidebarButtonIconBar'
+    sidebarButton.appendChild(iconBar.cloneNode(true))
+    sidebarButton.appendChild(iconBar.cloneNode(true))
+    sidebarButton.appendChild(iconBar.cloneNode(true))
+    sidebarButton.dataset.toggled = false
+    sidebarButton.addEventListener('click', () => {
+      this.toggleSidebar()
+    })
+    document.body.appendChild(sidebarButton)
+  }
+
+  reloadSidebar () {
+
+  }
+
+  toggleSidebar () {
+    let sidebarButton = document.querySelector('#sidebarButton')
+    sidebarButton.dataset.toggled = sidebarButton.dataset.toggled !== 'true'
+    document.documentElement.dataset.sidebarShown = sidebarButton.dataset.toggled
+    document.querySelector('#annotatorSidebarContainer').dataset.shown = sidebarButton.dataset.toggled
   }
 }
 
