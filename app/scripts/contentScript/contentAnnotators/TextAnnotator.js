830a8d59029304f947b85ed23d72197e4459d729
Haritz Medina <haritzmedina@gmail.com>
2017-11-23 12:18:20 +0100

    Fixed some minor bugs. Event listener destroy added.

--- app/scripts/contentScript/contentAnnotators/TextAnnotator.js
+++ app/scripts/contentScript/contentAnnotators/TextAnnotator.js
@@ -1,9 +1,44 @@
 const ContentAnnotator = require('./ContentAnnotator')
+const GroupSelector = require('../GroupSelector')
 const $ = require('jquery')
+const _ = require('lodash')
 
 class TextAnnotator extends ContentAnnotator {
-  init () {
-    document.addEventListener('mouseup', () => {
+  constructor () {
+    super()
+    this.events = {}
+    this.events.mouseUpOnDocumentHandler = null
+  }
+
+  init (callback) {
+    this.initSelectionEvents(() => {
+      this.loadAnnotations(() => {
+        if (_.isFunction(callback)) {
+          callback()
+        }
+      })
+    })
+  }
+
+  initSelectionEvents (callback) {
+    this.events.mouseUpOnDocumentHandler = this.mouseUpOnDocumentHandlerConstructor()
+    document.addEventListener('mouseup', this.events.mouseUpOnDocumentHandler)
+    if (_.isFunction(callback)) {
+      callback()
+    }
+  }
+
+  loadAnnotations () {
+    document.addEventListener(GroupSelector.eventGroupChange, this.hypothesisGroupChangedHandler, false)
+  }
+
+  hypothesisGroupChangedHandler (event) {
+    console.log('HypothesisGroupChanged')
+    console.log(event.detail)
+  }
+
+  mouseUpOnDocumentHandlerConstructor () {
+    return () => {
       // Check if something is selected
       if (document.getSelection().toString().length !== 0) {
         if ($(event.target).parents('#abwaSidebarWrapper').toArray().length === 0) {
@@ -17,7 +52,21 @@ class TextAnnotator extends ContentAnnotator {
           this.closeSidebar()
         }
       }
-    })
+    }
+  }
+
+  closeSidebar () {
+    super.closeSidebar()
+  }
+
+  openSidebar () {
+    super.openSidebar()
+  }
+
+  destroy () {
+    // Remove event listener
+    document.removeEventListener(GroupSelector.eventGroupChange, this.events.mouseUpOnDocumentHandler)
+    document.removeEventListener('mouseup', this.mouseUpOnDocumentHandler)
   }
 }
 
