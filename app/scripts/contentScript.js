bd2a3cf97c81ed07e639eb83a84628b6b976dbc7
Haritz Medina <haritzmedina@gmail.com>
2017-11-29 19:18:01 +0100

    Fixed loading content script more than once (for example in pdf files)

--- app/scripts/contentScript.js
+++ app/scripts/contentScript.js
@@ -1,17 +1,18 @@
 const ContentScriptManager = require('./contentScript/ContentScriptManager')
 
 const _ = require('lodash')
-
 if (_.isEmpty(window.abwa)) {
   window.abwa = {} // Global namespace for variables
-  window.abwa.contentScriptManager = new ContentScriptManager()
 
   window.addEventListener('load', () => {
     chrome.extension.onMessage.addListener((msg, sender, sendResponse) => {
-      if (msg.action === 'initContentScript') {
-        window.abwa.contentScriptManager.init()
-      } else if (msg.action === 'destroyContentScript') {
-        window.abwa.contentScriptManager.destroy()
+      if (_.isEmpty(window.abwa.contentScriptManager)) {
+        window.abwa.contentScriptManager = new ContentScriptManager()
+        if (msg.action === 'initContentScript') {
+          window.abwa.contentScriptManager.init()
+        } else if (msg.action === 'destroyContentScript') {
+          window.abwa.contentScriptManager.destroy()
+        }
       }
     })
   })
