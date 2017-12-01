7e15f8bf0d11420f7158c1e1b1a98b50e0b14d8b
Haritz Medina <haritzmedina@gmail.com>
2017-12-01 17:24:29 +0100

    Starting to save URL for PDF documents

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -44,6 +44,15 @@ class Background {
         }
       }
     })
+
+    // Initialize message manager
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      if (request.scope === 'extension') {
+        if (request.cmd === 'whoiam') {
+          sendResponse(sender)
+        }
+      }
+    })
   }
 }
 
--- app/scripts/contentScript/ContentTypeManager.js
+++ app/scripts/contentScript/ContentTypeManager.js
@@ -1,17 +1,43 @@
+const _ = require('lodash')
+const ChromeStorage = require('../utils/ChromeStorage')
+
 class ContentTypeManager {
   constructor () {
-    this.currentDocumentUri = window.location.href // TODO Remove hash and those things
+    this.currentDocumentUri = window.location.href
   }
 
   init (callback) {
     if (document.querySelector('embed[type="application/pdf"][name="plugin"]')) {
       console.log('Is pdf')
-      window.location = chrome.extension.getURL('content/pdfjs/web/viewer.html') + '?file=' + window.location.href
+      this.saveCurrentURI(() => {
+        window.location = chrome.extension.getURL('content/pdfjs/web/viewer.html') + '?file=' + window.location.href
+      })
     } else {
       if (_.isFunction(callback)) {
         callback()
       }
     }
+  }
+
+  saveCurrentURI (callback) {
+    // Retrieve current tab id
+    chrome.runtime.sendMessage({scope: 'extension', cmd: 'whoiam'}, (response) => {
+      // Current tab id
+      let tabId = response.tab.id
+      ChromeStorage.getData('ContentTypeManagerURI', ChromeStorage.local, (data) => {
+        if (data && data.urls) {
+          data.urls[tabId] = this.currentDocumentUri
+        } else {
+          data = data || {}
+          data.urls = data.urls || {}
+        }
+        ChromeStorage.setData('ContentTypeManager', data, ChromeStorage.local, () => {
+          if (_.isFunction(callback)) {
+            callback()
+          }
+        })
+      })
+    })
   }
 }
 
