0c80587f3094567b9d1894d8de409ce585316049
Haritz Medina <haritzmedina@gmail.com>
2017-12-17 01:19:21 +0100

    Refactored function of Hypothesis Group Initializer

--- app/scripts/googleSheetsContentScript/HypothesisGroupInitializer.js
+++ app/scripts/googleSheetsContentScript/HypothesisGroupInitializer.js
@@ -18,54 +18,75 @@ class HypothesisGroupInitializer {
       })
       // Create the group if not exists
       if (_.isEmpty(group)) {
-        this.createHypothesisGroup(() => {
-          if (_.isFunction(callback)) {
-            callback()
-          }
+        this.createHypothesisGroup((group) => {
+          this.createDimensionsAndCategories(group, () => {
+            this.createRelationGSheetGroup(group, () => {
+              prompt(chrome.i18n.getMessage('ShareHypothesisGroup'), group.url)
+              if (_.isFunction(callback)) {
+                callback()
+              }
+            })
+          })
         })
       } else {
+        prompt(chrome.i18n.getMessage('ShareHypothesisGroup'), group.url)
         // TODO Update Hypothesis group
       }
     })
   }
 
-  createHypothesisGroup () {
+  createHypothesisGroup (callback) {
     window.hag.hypothesisClientManager.hypothesisClient.createHypothesisGroup(this.parsedSheetData.title, (err, group) => {
       if (err) {
         console.error(err) // TODO Show to the user the error
-      }
-      console.debug('Created group in hypothesis: ')
-      console.debug(group)
-      // Create dimensions and categories annotations
-      let dimensionsAndCategoriesPairs = _.toPairs(this.parsedSheetData.dimensions)
-      let annotations = []
-      for (let i = 0; i < dimensionsAndCategoriesPairs.length; i++) {
-        let pair = dimensionsAndCategoriesPairs[i]
-        // Create dimension annotation
-        let dimensionName = pair[0]
-        annotations.push(this.generateAnnotationCorpus(group, ['slr:dimension:' + dimensionName]))
-        // Create categories annotation
-        let categories = pair[1]
-        for (let j = 0; j < categories.length; j++) {
-          let categoryName = categories[j]
-          annotations.push(this.generateAnnotationCorpus(
-            group,
-            ['slr:category:' + categoryName, 'slr:isCategoryOf:' + dimensionName]))
+      } else {
+        console.debug('Created group in hypothesis: ')
+        console.debug(group)
+        if (_.isFunction(callback)) {
+          callback(group)
         }
       }
-      console.debug('Generated dimensions and categories annotations: ')
-      console.debug(annotations)
-      window.hag.hypothesisClientManager.hypothesisClient.createNewAnnotations(annotations, (err, result) => {
-        if (err) {
-          console.error(err) // TODO Show to the user the error
-        } else {
-          // TODO Create relation to sheet annotation
-          let relationAnnotation = this.generateRelateSheetAndGroupAnnotation(this.parsedSheetData.gSheetId, group)
-          window.hag.hypothesisClientManager.hypothesisClient.createNewAnnotation(relationAnnotation, (response) => {
-            prompt(chrome.i18n.getMessage('ShareHypothesisGroup'), group.url)
-          })
+    })
+  }
+
+  createDimensionsAndCategories (group, callback) {
+    // Create dimensions and categories annotations
+    let dimensionsAndCategoriesPairs = _.toPairs(this.parsedSheetData.dimensions)
+    let annotations = []
+    for (let i = 0; i < dimensionsAndCategoriesPairs.length; i++) {
+      let pair = dimensionsAndCategoriesPairs[i]
+      // Create dimension annotation
+      let dimensionName = pair[0]
+      annotations.push(this.generateAnnotationCorpus(group, ['slr:dimension:' + dimensionName]))
+      // Create categories annotation
+      let categories = pair[1]
+      for (let j = 0; j < categories.length; j++) {
+        let categoryName = categories[j]
+        annotations.push(this.generateAnnotationCorpus(
+          group,
+          ['slr:category:' + categoryName, 'slr:isCategoryOf:' + dimensionName]))
+      }
+    }
+    console.debug('Generated dimensions and categories annotations: ')
+    console.debug(annotations)
+    window.hag.hypothesisClientManager.hypothesisClient.createNewAnnotations(annotations, (err, result) => {
+      if (err) {
+        console.error(err) // TODO Show to the user the error
+      } else {
+        if (_.isFunction(callback)) {
+          callback()
         }
-      })
+      }
+    })
+  }
+
+  createRelationGSheetGroup (group, callback) {
+    // Create relation to sheet annotation
+    let relationAnnotation = this.generateRelateSheetAndGroupAnnotation(this.parsedSheetData.gSheetId, group)
+    window.hag.hypothesisClientManager.hypothesisClient.createNewAnnotation(relationAnnotation, (response) => {
+      if (_.isFunction(callback)) {
+        callback()
+      }
     })
   }
 
