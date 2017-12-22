7dfeae7ea267ffbda24124034eb8a1f06a6c7923
Haritz Medina <haritzmedina@gmail.com>
2017-12-22 18:50:36 +0100

    Some color util functions added

--- app/scripts/utils/ColorUtils.js
+++ app/scripts/utils/ColorUtils.js
@@ -1,5 +1,6 @@
 const ColorHash = require('color-hash')
 const Color = require('color')
+const _ = require('lodash')
 
 class ColorUtils {
   static getRandomColor () {
@@ -13,15 +14,47 @@ class ColorUtils {
     return 'rgba(' + red + ',' + green + ',' + blue + ', ' + alpha + ')'
   }
 
-  static getHashColor (text) {
-    let colorHash = new ColorHash()
+  static getHashColor (text, alpha) {
+    let colorHash = new ColorHash({hash: ColorUtils.customHash})
     let resultArray = colorHash.rgb(text)
-    let alpha = 0.5
-    return 'rgba(' + resultArray[0] + ',' + resultArray[1] + ',' + resultArray[2] + ', ' + alpha + ')'
+    let alphaValue = alpha || 0.5
+    return 'rgba(' + resultArray[0] + ',' + resultArray[1] + ',' + resultArray[2] + ', ' + alphaValue + ')'
   }
 
   static setAlphaToColor (color, alpha) {
     return Color(color).alpha(alpha).rgb().string()
+  }
+
+  static customHash (str) {
+    let hash = 0
+    for (let i = 0; i < str.length; i++) {
+      let char = str.charCodeAt(i)
+      hash += char
+    }
+    return hash
+  }
+
+  static hasAlpha (str) {
+    let color = new Color(str)
+    return color.valpha !== 1
+  }
+
+  static getDifferentColor (colorList, str) {
+    let color = null
+    let attempts = 0
+    do {
+      color = ColorUtils.getHashColor(str)
+      if (_.find(colorList, (c) => { return (new Color(c)).contrast(new Color(color)) < 2 })) {
+        color = null
+        str += 'a'
+        attempts += 1 // Another try
+      }
+    } while (_.isEmpty(color) && attempts < 10)
+    return color
+  }
+
+  static isDark (colorStr) {
+
   }
 }
 
