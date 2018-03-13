haritzmedina/MarkAndGo

commit 2b6b4e50309189617be2767005fd6e92317d7edf

Callback of getProfile of hypothesis client support error handling.

app/scripts/contentScript/GroupSelector.js
@@ -43,13 +43,19 @@ class GroupSelector {
     if (window.abwa.annotationBasedInitializer.initAnnotation) {
       let annotationGroupId = window.abwa.annotationBasedInitializer.initAnnotation.group
       // Load group of annotation
-      this.retrieveHypothesisGroups((groups) => {
-        // Set current group
-        this.currentGroup = _.find(groups, (group) => { return group.id === annotationGroupId })
-        // Save to chrome storage current group
-        ChromeStorage.setData(selectedGroupNamespace, {data: JSON.stringify(this.currentGroup)}, ChromeStorage.local)
-        if (_.isFunction(callback)) {
-          callback()
+      this.retrieveHypothesisGroups((err, groups) => {
+        if (err) {
+          if (_.isFunction(callback)) {
+            callback(err)
+          }
+        } else {
+          // Set current group
+          this.currentGroup = _.find(groups, (group) => { return group.id === annotationGroupId })
+          // Save to chrome storage current group
+          ChromeStorage.setData(selectedGroupNamespace, {data: JSON.stringify(this.currentGroup)}, ChromeStorage.local)
+          if (_.isFunction(callback)) {
+            callback()
+          }
         }
       })
     } else { // If initialization annotation is not set
@@ -132,10 +138,16 @@ class GroupSelector {
   }
 
   retrieveHypothesisGroups (callback) {
-    window.abwa.hypothesisClientManager.hypothesisClient.getUserProfile((profile) => {
-      this.user = profile
-      if (_.isFunction(callback)) {
-        callback(profile.groups)
+    window.abwa.hypothesisClientManager.hypothesisClient.getUserProfile((err, profile) => {
+      if (err) {
+        if (_.isFunction(callback)) {
+          callback(err)
+        }
+      } else {
+        this.user = profile
+        if (_.isFunction(callback)) {
+          callback(null, profile.groups)
+        }
       }
     })
   }

app/scripts/hypothesis/HypothesisClient.js
@@ -126,7 +126,7 @@ class HypothesisClient {
       }
     }
     $.ajax(settings).done((response) => {
-      callback(response)
+      callback(null, response)
     })
   }
 
@@ -313,7 +313,7 @@ Content-Disposition: form-data; name="submit"
 submit
 -----------------------------sep--
 `
-        let req = new XMLHttpRequest()
+        let req = new window.XMLHttpRequest()
         req.open('POST', 'https://hypothes.is/groups/new', true)
         req.setRequestHeader('Content-Type', 'multipart/form-data; boundary=---------------------------sep')
         req.onload = function () {

app/scripts/contentScript/TagManager.js
@@ -115,7 +115,7 @@ class TagManager {
       order: 'desc'
     }, (err, annotations) => {
       if (err) {
-        alert('Unable to retrieve document annotations')
+        window.alert('Unable to retrieve document annotations') // TODO Swal
       } else {
         // Retrieve tags which has the namespace
         annotations = _.filter(annotations, (annotation) => {

haritzmedina/highlightAndGo

commit 4e767e41fba2b3b9e798e94361f3cf86bdbfcdd4

Callback of getProfile of Hypothes.is and HypothesisClient searchAnnotations bug on double call to the API when no required.
From mark&Go fixes: 2b6b4e50309189617be2767005fd6e92317d7edf and 987d166cab916a0fdfbb40f2bca2a9dcb9899faa

app/scripts/contentScript/GroupSelector.js
@@ -43,13 +43,19 @@ class GroupSelector {
     if (window.abwa.annotationBasedInitializer.initAnnotation) {
       let annotationGroupId = window.abwa.annotationBasedInitializer.initAnnotation.group
       // Load group of annotation
-      this.retrieveHypothesisGroups((groups) => {
-        // Set current group
-        this.currentGroup = _.find(groups, (group) => { return group.id === annotationGroupId })
-        // Save to chrome storage current group
-        ChromeStorage.setData(selectedGroupNamespace, {data: JSON.stringify(this.currentGroup)}, ChromeStorage.local)
-        if (_.isFunction(callback)) {
-          callback()
+      this.retrieveHypothesisGroups((err, groups) => {
+        if (err) {
+          if (_.isFunction(callback)) {
+            callback(err)
+          }
+        } else {
+          // Set current group
+          this.currentGroup = _.find(groups, (group) => { return group.id === annotationGroupId })
+          // Save to chrome storage current group
+          ChromeStorage.setData(selectedGroupNamespace, {data: JSON.stringify(this.currentGroup)}, ChromeStorage.local)
+          if (_.isFunction(callback)) {
+            callback()
+          }
         }
       })
     } else { // If initialization annotation is not set
@@ -57,7 +63,9 @@ class GroupSelector {
         // Retrieve last saved group
         ChromeStorage.getData(selectedGroupNamespace, ChromeStorage.local, (err, savedCurrentGroup) => {
           if (err) {
-            throw new Error('Unable to retrieve current selected group')
+            if (_.isFunction(callback)) {
+              callback(new Error('Unable to retrieve current selected group'))
+            }
           } else {
             // Parse chrome storage result
             if (!_.isEmpty(savedCurrentGroup) && savedCurrentGroup.data) {
@@ -65,9 +73,9 @@ class GroupSelector {
             } else {
               this.currentGroup = defaultGroup
             }
-          }
-          if (_.isFunction(callback)) {
-            callback()
+            if (_.isFunction(callback)) {
+              callback()
+            }
           }
         })
       } else {
@@ -132,10 +140,16 @@ class GroupSelector {
   }
 
   retrieveHypothesisGroups (callback) {
-    window.abwa.hypothesisClientManager.hypothesisClient.getUserProfile((profile) => {
-      this.user = profile
-      if (_.isFunction(callback)) {
-        callback(profile.groups)
+    window.abwa.hypothesisClientManager.hypothesisClient.getUserProfile((err, profile) => {
+      if (err) {
+        if (_.isFunction(callback)) {
+          callback(err)
+        }
+      } else {
+        this.user = profile
+        if (_.isFunction(callback)) {
+          callback(null, profile.groups)
+        }
       }
     })
   }

app/scripts/hypothesis/HypothesisClient.js
@@ -126,7 +126,7 @@ class HypothesisClient {
       }
     }
     $.ajax(settings).done((response) => {
-      callback(response)
+      callback(null, response)
     })
   }
 
@@ -192,46 +192,53 @@ class HypothesisClient {
     let annotations = []
     this.searchBunchAnnotations(data, 0, (err, response) => {
       if (err) {
-        console.error('Unable to retrieve annotations')
+        console.error('Unable to retrieve annotations from Hypothes.is')
+        callback(new Error('Unable to retrieve annotations from Hypothes.is. Check internet connection and permissions.'))
       } else {
         // Concat first time done annotations
         annotations = annotations.concat(response.rows)
-        // Set maximum of queries
-        let total = data.limit || response.total
-        if (total > MAX_NUMBER_OF_ANNOTATIONS_TO_SEARCH) {
-          total = MAX_NUMBER_OF_ANNOTATIONS_TO_SEARCH // Limit the number of results
-        }
-        // Retrieve the rest of annotations
-        let promises = []
-        for (let i = annotations.length; i < total; i += 200) {
-          let iterationData = Object.assign({}, data)
-          if (total < i + 200) {
-            iterationData.limit = total % 200
-          } else {
-            iterationData.limit = 200
-          }
-          // Create a promise for each request to do
-          promises.push(new Promise((resolve, reject) => {
-            this.searchBunchAnnotations(iterationData, i, (err, response) => {
-              if (err) {
-                reject(new Error(err)) // TODO Manage error
-              } else {
-                annotations = annotations.concat(response.rows)
-                resolve()
-              }
-            })
-          }))
-        }
-        // Execute all the promises
-        Promise.all(promises).catch((reasons) => {
+        if (response.total === response.rows.length) { // If already retrieved results are all, no required to do more calls
           if (_.isFunction(callback)) {
-            callback(new Error('Unable to retrieve annotations'))
+            callback(null, response.rows)
           }
-        }).then(() => {
-          if (_.isFunction(callback)) {
-            callback(null, annotations)
+        } else {
+          // Set maximum of queries
+          let total = data.limit || response.total
+          if (total > MAX_NUMBER_OF_ANNOTATIONS_TO_SEARCH) {
+            total = MAX_NUMBER_OF_ANNOTATIONS_TO_SEARCH // Limit the number of results
           }
-        })
+          // Retrieve the rest of annotations
+          let promises = []
+          for (let i = annotations.length; i < total; i += 200) {
+            let iterationData = Object.assign({}, data)
+            if (total < i + 200) {
+              iterationData.limit = total % 200
+            } else {
+              iterationData.limit = 200
+            }
+            // Create a promise for each request to do
+            promises.push(new Promise((resolve, reject) => {
+              this.searchBunchAnnotations(iterationData, i, (err, response) => {
+                if (err) {
+                  reject(new Error(err)) // TODO Manage error
+                } else {
+                  annotations = annotations.concat(response.rows)
+                  resolve()
+                }
+              })
+            }))
+          }
+          // Execute all the promises
+          Promise.all(promises).catch((reasons) => {
+            if (_.isFunction(callback)) {
+              callback(new Error('Unable to retrieve annotations'))
+            }
+          }).then(() => {
+            if (_.isFunction(callback)) {
+              callback(null, annotations)
+            }
+          })
+        }
       }
     })
   }
@@ -313,7 +320,7 @@ Content-Disposition: form-data; name="submit"
 submit
 -----------------------------sep--
 `
-        let req = new XMLHttpRequest()
+        let req = new window.XMLHttpRequest()
         req.open('POST', 'https://hypothes.is/groups/new', true)
         req.setRequestHeader('Content-Type', 'multipart/form-data; boundary=---------------------------sep')
         req.onload = function () {