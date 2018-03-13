736fbca18d942d46d2b22bbf9f1b8ab6bb76d155
Haritz Medina <haritzmedina@gmail.com>
2018-03-13 13:24:20 +0100

    Updated configuration to add exams use case

--- app/scripts/Config.js
+++ app/scripts/Config.js
@@ -8,21 +8,44 @@ const Config = {
     tags: {}
   },
   slrDataExtraction: {
-    contentAnnotator: 'text',
-    namespace: 'slr',
+    contentAnnotator: 'text', // Type of content annotator
+    namespace: 'slr', // Namespace for the annotations
     sidebar: {},
-    location: true,
-    tags: {
-      grouped: {
+    location: true, // Location mode
+    userFilter: true,
+    tags: { // Defined tags for the domain
+      grouped: { // Grouped annotations
         group: 'facet',
         subgroup: 'code',
         relation: 'isCodeOf'
       },
-      statics: {
+      statics: { // Other static tags specific for the domain
         multivalued: 'multivalued',
         inductive: 'inductive',
         validated: 'validated',
         spreadsheet: 'spreadsheet'
+      }
+    }
+  },
+  exams: {
+    contentAnnotator: 'text',
+    namespace: 'exam',
+    sidebar: {},
+    location: true,
+    pattern: '',
+    tags: { // Defined tags for the domain
+      grouped: { // Grouped annotations
+        group: 'criteria',
+        subgroup: 'mark',
+        relation: 'isCriteriaOf'
+      },
+      statics: { // Other static tags specific for the domain
+        multivalued: 'multivalued',
+        inductive: 'inductive',
+        validated: 'validated',
+        spreadsheet: 'spreadsheet',
+        teacher: 'teacher',
+        reviewed: 'reviewed'
       }
     }
   }
